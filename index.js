4ac0c2cf9d4271d71a44b03f4528088c1f79d510
ricardo <ricklimadealmeida@gmail.com>
2021-04-06 07:49:40 -0300

    netflix partialy implemented

--- index.js
+++ index.js
@@ -10,7 +10,18 @@ slider.addEventListener('mousedown', (e) => {
     scrollLeft = slider.scrollLeft
 })
 
+slider.addEventListener(TouchEvent, (e) => {
+    isDown = true
+    slider.classList.add('active')
+    startX = e.pageX - slider.offsetLeft
+    scrollLeft = slider.scrollLeft
+})
+
 slider.addEventListener('mouseleave', () => {
+    isDown = false
+    slider.classList.remove('active')
+})
+slider.addEventListener(Touch, () => {
     isDown = false
     slider.classList.remove('active')
 })
@@ -20,7 +31,19 @@ slider.addEventListener('mouseup', () => {
     slider.classList.remove('active')
 })
 
+
+slider.addEventListener(Touch, () => {
+    isDown = false
+    slider.classList.remove('active')
+})
 slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return  // stop the fn from running
+    e.preventDefault()
+    const x = e.pageX - slider.offsetLeft
+    const walk = (x - startX) * 1
+    slider.scrollLeft = scrollLeft - walk
+})
+slider.addEventListener(TouchEvent, (e) => {
     if (!isDown) return  // stop the fn from running
     e.preventDefault()
     const x = e.pageX - slider.offsetLeft
